fb50d9afc07d67456368a0e212af898947c3f1b1
Jomari <109713343+Noyzki@users.noreply.github.com>
2024-12-05 19:46:31 +0800

    issue-ticket = add passenger type dropdown

--- src/core/issueticket/issue_ticket.js
+++ src/core/issueticket/issue_ticket.js
@@ -11,7 +11,7 @@ function IssueTicket() {
   const [passengerName, setPassengerName] = useState('');
   const [contactNumber, setContactNumber] = useState('');
   const [email, setEmail] = useState('');
-  const [numberOfTickets, setNumberOfTickets] = useState(1);
+  const [passengerType, setPassengerType] = useState('adult');
 
   useEffect(() => {
     const fetchTrips = async () => {
@@ -54,10 +54,11 @@ function IssueTicket() {
       passenger: {
         name: passengerName,
         contact: contactNumber,
+        type: passengerType,
       },
       ticket_number: 'TICKET1234',
       seat_number: '1A',
-      age_group: 'adult',
+      age_group: passengerType,
       price: '1999',
       discount: '',
       baggage_ticket: true,
@@ -129,13 +130,17 @@ function IssueTicket() {
                   />
                 </label>
                 <label>
-                  Number of Tickets:
-                  <input
-                    type="number"
-                    placeholder="Number"
-                    value={numberOfTickets}
-                    onChange={(e) => setNumberOfTickets(e.target.value)}
-                  />
+                  Passenger:
+                  <select
+                    value={passengerType}
+                    onChange={(e) => setPassengerType(e.target.value)}
+                  >
+                    <option value="adult">Adult</option>
+                    <option value="child">Child - (4-10 yrs old)</option>
+                    <option value="senior">Senior - (65 yrs old)</option>
+                    <option value="student">Student</option>
+                    <option value="infant">Infant - (1-3 yrs old)</option>
+                  </select>
                 </label>
                 <hr />
                 <p></p>
@@ -149,7 +154,7 @@ function IssueTicket() {
                   className="generate-btn"
                   onClick={handleGenerateTicket}
                 >
-                  Generate Ticket
+                  Add Ticket
                 </button>
                 {error && <p className="text-danger">{error}</p>}
               </form>
