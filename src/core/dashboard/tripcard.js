8414981fff44e987d75e8b8ae23f32312c48c250
Jomari <109713343+Noyzki@users.noreply.github.com>
2024-12-05 17:20:49 +0800

    dashboard ui modify

--- src/core/dashboard/tripcard.js
+++ src/core/dashboard/tripcard.js
@@ -5,13 +5,12 @@ function Tripcard({ from, destination, boatImage, date = "12/25/2024", boatNumbe
   return (
     <div className="card empty-card-tripcard">
       <div className="card-body-tripcard">
-        <input type="checkbox" className="card-checkbox-tripcard" />
         <img src={boatImage} alt="Boat Logo" className="boat-logo-tripcard" />
         <div className="from-component-tripcard">
           <p className="from-text-tripcard">From</p>
           <h4 className="from-destination-tripcard">{from}</h4>
         </div>
-        <div className="separator-tripcard" />
+        <div className="separator-tripcard mt-3" />
         <div className="to-component-tripcard">
           <p className="to-text-tripcard">To</p>
           <h4 className="to-destination-tripcard">{destination}</h4>
@@ -23,7 +22,7 @@ function Tripcard({ from, destination, boatImage, date = "12/25/2024", boatNumbe
           <h4 className="departure-capacity-tripcard">150</h4>
         </div>
         <div>
-          <button type="button" className="view-details-button-tripcard">
+          <button type="button" className="view-details-button-tripcard mt-3">
             View Details
           </button>
         </div>
